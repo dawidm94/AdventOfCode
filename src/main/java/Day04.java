6c01d4dfaa1e9db218a20b607a1ee23db71ddb21
Dawid Marcinków <31923182+dawidm94@users.noreply.github.com>
2019-12-04 10:59:06 +0100

    Day 4

---
+++ src/main/java/Day04.java
@@ -0,0 +1,48 @@
+package pl.ingbank.spk.acq;
+
+public class Day04 {
+
+    public static void main(String[] args) {
+        int startRange = 240298;
+        int endRange = 784956;
+        int counter = 0;
+
+        for (int i = startRange; i <= endRange; i++) {
+            if(factsAboutPasswordAreCorrect(i)) {
+                counter++;
+            }
+        }
+        System.out.println("Possible passwords: " + counter);
+    }
+
+    private static boolean factsAboutPasswordAreCorrect(int number) {
+        String integerWord = Integer.toString(number);
+        boolean adjacentDigitsAreTheSame = false;
+        boolean thereAreOnlyTwoSameAdjacentDigits = false;
+        boolean digitsNeverDecrease = true;
+
+        char[] digitsArray = integerWord.toCharArray();
+        for (int i=1; i<digitsArray.length; i++) {
+//            PART 1
+//            if (digitsArray[i-1] == digitsArray[i]) {
+//                adjacentDigitsAreTheSame = true;
+//            }
+
+//            PART 2
+            if ((digitsArray[i-1] == digitsArray[i])) {
+                int wordLength = integerWord.length();
+                int wordLengthWithoutCurrentChar = integerWord.replaceAll(Character.toString(digitsArray[i]), "").length();
+                adjacentDigitsAreTheSame = true;
+                if (wordLength - wordLengthWithoutCurrentChar == 2) {
+                    thereAreOnlyTwoSameAdjacentDigits = true;
+                }
+            }
+
+            if (Character.getNumericValue(digitsArray[i-1]) > Character.getNumericValue(digitsArray[i])) {
+                digitsNeverDecrease = false;
+                break;
+            }
+        }
+        return adjacentDigitsAreTheSame && digitsNeverDecrease && thereAreOnlyTwoSameAdjacentDigits;
+    }
+}
