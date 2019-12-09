4765413e5219b54d1436b3b458c7c2c77ceaaf21
Dawid Marcinków <dawid.marcinkow@gmail.com>
2019-12-09 22:35:51 +0100

    Day 5 - refactor

--- src/main/java/Day05_2.java
+++ src/main/java/Day05_2.java
@@ -1,116 +1,123 @@
 package main.java;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day05_2 {
 
-    private static List<Integer> input = Arrays
-        .asList(3,225,1,225,6,6,1100,1,238,225,104,0,1101,40,71,224,1001,224,-111,224,4,224,1002,223,8,223,101,7,224,224,1,224,223,223,1102,66,6,225,1102,22,54,225,1,65,35,224,1001,224,-86,224,4,224,102,8,223,223,101,6,224,224,1,224,223,223,1102,20,80,225,101,92,148,224,101,-162,224,224,4,224,1002,223,8,223,101,5,224,224,1,224,223,223,1102,63,60,225,1101,32,48,225,2,173,95,224,1001,224,-448,224,4,224,102,8,223,223,1001,224,4,224,1,224,223,223,1001,91,16,224,101,-79,224,224,4,224,1002,223,8,223,101,3,224,224,1,224,223,223,1101,13,29,225,1101,71,70,225,1002,39,56,224,1001,224,-1232,224,4,224,102,8,223,223,101,4,224,224,1,223,224,223,1101,14,59,225,102,38,143,224,1001,224,-494,224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1102,30,28,224,1001,224,-840,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,107,677,226,224,1002,223,2,223,1005,224,329,1001,223,1,223,8,226,226,224,102,2,223,223,1006,224,344,101,1,223,223,7,226,677,224,1002,223,2,223,1005,224,359,101,1,223,223,1007,677,226,224,1002,223,2,223,1005,224,374,1001,223,1,223,1007,677,677,224,1002,223,2,223,1006,224,389,101,1,223,223,1008,226,226,224,1002,223,2,223,1005,224,404,1001,223,1,223,108,677,226,224,1002,223,2,223,1006,224,419,1001,223,1,223,1108,677,226,224,102,2,223,223,1006,224,434,1001,223,1,223,108,226,226,224,1002,223,2,223,1005,224,449,101,1,223,223,7,677,677,224,1002,223,2,223,1006,224,464,1001,223,1,223,8,226,677,224,1002,223,2,223,1005,224,479,1001,223,1,223,107,226,226,224,102,2,223,223,1006,224,494,101,1,223,223,1007,226,226,224,1002,223,2,223,1005,224,509,1001,223,1,223,1107,226,677,224,102,2,223,223,1005,224,524,1001,223,1,223,108,677,677,224,1002,223,2,223,1005,224,539,101,1,223,223,1107,677,226,224,102,2,223,223,1005,224,554,1001,223,1,223,107,677,677,224,1002,223,2,223,1005,224,569,101,1,223,223,8,677,226,224,102,2,223,223,1005,224,584,1001,223,1,223,7,677,226,224,102,2,223,223,1006,224,599,101,1,223,223,1008,677,677,224,1002,223,2,223,1005,224,614,101,1,223,223,1008,677,226,224,102,2,223,223,1006,224,629,1001,223,1,223,1108,677,677,224,102,2,223,223,1006,224,644,101,1,223,223,1108,226,677,224,1002,223,2,223,1005,224,659,1001,223,1,223,1107,226,226,224,102,2,223,223,1006,224,674,1001,223,1,223,4,223,99,226);
+	private static List<Integer> input = Arrays
+			.asList(3,225,1,225,6,6,1100,1,238,225,104,0,1101,40,71,224,1001,224,-111,224,4,224,1002,223,8,223,101,7,224,224,1,224,223,223,1102,66,6,225,1102,22,54,225,1,65,35,224,1001,224,-86,224,4,224,102,8,223,223,101,6,224,224,1,224,223,223,1102,20,80,225,101,92,148,224,101,-162,224,224,4,224,1002,223,8,223,101,5,224,224,1,224,223,223,1102,63,60,225,1101,32,48,225,2,173,95,224,1001,224,-448,224,4,224,102,8,223,223,1001,224,4,224,1,224,223,223,1001,91,16,224,101,-79,224,224,4,224,1002,223,8,223,101,3,224,224,1,224,223,223,1101,13,29,225,1101,71,70,225,1002,39,56,224,1001,224,-1232,224,4,224,102,8,223,223,101,4,224,224,1,223,224,223,1101,14,59,225,102,38,143,224,1001,224,-494,224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1102,30,28,224,1001,224,-840,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,107,677,226,224,1002,223,2,223,1005,224,329,1001,223,1,223,8,226,226,224,102,2,223,223,1006,224,344,101,1,223,223,7,226,677,224,1002,223,2,223,1005,224,359,101,1,223,223,1007,677,226,224,1002,223,2,223,1005,224,374,1001,223,1,223,1007,677,677,224,1002,223,2,223,1006,224,389,101,1,223,223,1008,226,226,224,1002,223,2,223,1005,224,404,1001,223,1,223,108,677,226,224,1002,223,2,223,1006,224,419,1001,223,1,223,1108,677,226,224,102,2,223,223,1006,224,434,1001,223,1,223,108,226,226,224,1002,223,2,223,1005,224,449,101,1,223,223,7,677,677,224,1002,223,2,223,1006,224,464,1001,223,1,223,8,226,677,224,1002,223,2,223,1005,224,479,1001,223,1,223,107,226,226,224,102,2,223,223,1006,224,494,101,1,223,223,1007,226,226,224,1002,223,2,223,1005,224,509,1001,223,1,223,1107,226,677,224,102,2,223,223,1005,224,524,1001,223,1,223,108,677,677,224,1002,223,2,223,1005,224,539,101,1,223,223,1107,677,226,224,102,2,223,223,1005,224,554,1001,223,1,223,107,677,677,224,1002,223,2,223,1005,224,569,101,1,223,223,8,677,226,224,102,2,223,223,1005,224,584,1001,223,1,223,7,677,226,224,102,2,223,223,1006,224,599,101,1,223,223,1008,677,677,224,1002,223,2,223,1005,224,614,101,1,223,223,1008,677,226,224,102,2,223,223,1006,224,629,1001,223,1,223,1108,677,677,224,102,2,223,223,1006,224,644,101,1,223,223,1108,226,677,224,1002,223,2,223,1005,224,659,1001,223,1,223,1107,226,226,224,102,2,223,223,1006,224,674,1001,223,1,223,4,223,99,226
+			);
+	private static List<Long> inputList;
+	private static final Long INPUT_VALUE = 5L;
 
-    private static final Integer DEFAULT_INPUT_INTEGER = 5;
+	public static void main(String[] args) throws Exception {
+		inputList = parseIntListToLongList(input);
+		calculate(inputList);
+	}
 
-    public static void main(String[] args) throws Exception {
-        for (Integer i = 0; i < input.size(); i++) {
-            Integer firstParam;
-            Integer secondParam;
-            Integer thirdParam;
-            Integer nextParamValue;
-            Integer modeOfFirstParam = 0;
-            Integer modeOfSecondParam = 0;
-            Integer modeOfThirdParam = 0;
-            boolean isInstruction;
+	private static List<Long> parseIntListToLongList(List<Integer> list) {
+		return list.stream()
+				.mapToLong(Integer::longValue)
+				.boxed()
+				.collect(Collectors.toList());
+	}
 
-            if (input.get(i) == 99) {
-                break;
+	private static void calculate(List<Long> inputList) {
+		int i=0, length = inputList.size();
+		dance: while (i < length) {
+			Long nexValue = inputList.get(i);
+			String instruction = String.format("%05d", nexValue).replaceAll("-","0");
+			int firstParamMode = Integer.parseInt(instruction.substring(2, 3));
+			int secondParamMode = Integer.parseInt(instruction.substring(1,2));
+//			int thirdParamMode = Integer.parseInt(instruction.substring(0,1));
+			int opCode = Integer.parseInt(instruction.substring(3,5));
+			Long firstParam, secondParam, thirdParam, thirdParamValue;
 
-            } else {
-                Integer oppcode = input.get(i);
+			switch (opCode) {
+				case 1:
+					firstParam = getParam(inputList.get(i+1), firstParamMode);
+					secondParam = getParam(inputList.get(i+2), secondParamMode);
+					thirdParamValue = inputList.get(i+3);
+					inputList.set(thirdParamValue.intValue(), firstParam+secondParam);
+					i+=4;
+					break;
 
-                if (oppcode.toString().length() > 2) {
-                    String instruction = String.format("%05d", oppcode).replaceAll("-","0");
-                    modeOfThirdParam = Integer.parseInt(instruction.substring(0,1));
-                    modeOfSecondParam = Integer.parseInt(instruction.substring(1,2));
-                    modeOfFirstParam = Integer.parseInt(instruction.substring(2, 3));
-                    oppcode = Integer.parseInt(instruction.substring(3,5));
-                    isInstruction = true;
+				case 2:
+					firstParam = getParam(inputList.get(i+1), firstParamMode);
+					secondParam = getParam(inputList.get(i+2), secondParamMode);
+					thirdParamValue = inputList.get(i+3);
+					inputList.set(thirdParamValue.intValue(), firstParam*secondParam);
+					i+=4;
+					break;
 
-                } else {
-                    isInstruction = false;
-                }
+				case 3:
+					firstParam = inputList.get(i+1);
+					inputList.set(firstParam.intValue(), INPUT_VALUE);
+					i+=2;
+					break;
 
-                firstParam = getValueAtIndex(input.get(i+1), modeOfFirstParam == 1);
-                nextParamValue = input.get(i+1);
+				case 4:
+					firstParam = getParam(inputList.get(i+1), firstParamMode);
+					System.out.println("OUTPUT: " + firstParam);
+					i+=2;
+					break;
 
-
-                if (oppcode == 1 || oppcode == 2 || oppcode == 7 || oppcode == 8) {
-                    secondParam = getValueAtIndex(input.get(i+2), modeOfSecondParam == 1);
-                    thirdParam = modeOfThirdParam == 1 ? i+3 : input.get(i+3);
-
-                    if (oppcode == 1) {
-                        input.set(thirdParam, firstParam + secondParam);
-                    } else if (oppcode == 2) {
-                        input.set(thirdParam, firstParam * secondParam);
-                    } else if (oppcode == 7) {
-                    	if (firstParam < secondParam) {
-                    		input.set(thirdParam, 1);
-						} else {
-                    		input.set(thirdParam, 0);
-						}
-					} else if (oppcode == 8) {
-                    	if (firstParam.equals(secondParam)) {
-                    		input.set(thirdParam, 1);
-						} else {
-                    		input.set(thirdParam, 0);
-						}
-					}
-
-                } else if (oppcode == 3) {
-                    input.set(nextParamValue, DEFAULT_INPUT_INTEGER);
-
-                } else if (oppcode == 4) {
-                    Integer inputValue = getValueAtIndex(nextParamValue, isInstruction);
-                    System.out.println(inputValue);
-
-                }  else if (oppcode == 5) {
-					secondParam = getValueAtIndex(input.get(i+2), modeOfSecondParam == 1);
+				case 5:
+					firstParam = getParam(inputList.get(i+1), firstParamMode);
 					if (firstParam != 0) {
-                    	i = secondParam -1;
+						i = getParam(inputList.get(i+2), secondParamMode).intValue();
 					} else {
-						i+=2;
+						i+=3;
 					}
-                    	continue;
+					break;
 
-                }  else if (oppcode == 6) {
-					secondParam = getValueAtIndex(input.get(i+2), modeOfSecondParam == 1);
-
+				case 6:
+					firstParam = getParam(inputList.get(i+1), firstParamMode);
 					if (firstParam == 0) {
-						i = secondParam -1;
+						i = getParam(inputList.get(i+2), secondParamMode).intValue();
 					} else {
-						i+=2;
+						i+=3;
 					}
-					continue;
+					break;
 
-                } else {
-                    throw new Exception("Wrong oppcode (" + oppcode + ") at " + i + "line.");
+				case 7:
+					firstParam = getParam(inputList.get(i+1), firstParamMode);
+					secondParam = getParam(inputList.get(i+2), secondParamMode);
+					thirdParam = inputList.get(i+3);
+					if (firstParam < secondParam) {
+						inputList.set(thirdParam.intValue(), 1L);
+					} else {
+						inputList.set(thirdParam.intValue(), 0L);
+					}
+					i+=4;
+					break;
 
-                }
-                i = increaseIndex(i, oppcode);
-            }
-        }
-    }
+				case 8:
+					firstParam = getParam(inputList.get(i+1), firstParamMode);
+					secondParam = getParam(inputList.get(i+2), secondParamMode);
+					thirdParam = inputList.get(i+3);
+					if (firstParam.equals(secondParam)) {
+						inputList.set(thirdParam.intValue(), 1L);
+					} else {
+						inputList.set(thirdParam.intValue(), 0L);
+					}
+					i+=4;
+					break;
 
-    private static Integer increaseIndex(Integer index, Integer oppcode) {
+				case 99:
+					break dance; // (° ͜ʖ °)
+			}
+		}
+	}
 
-        if (oppcode == 1 || oppcode == 2 || oppcode == 7 || oppcode == 8) {
-            index += 3;
-        } else {
-            index += 1;
-        }
-        return index;
-    }
-
-    private static Integer getValueAtIndex(Integer index, boolean isImmediateMode) {
-        return isImmediateMode ? index : input.get(index);
-    }
+	private static Long getParam(Long index, int paramMode) {
+		if (paramMode == 0) {
+			return inputList.get(index.intValue());
+		} else {
+			return index;
+		}
+	}
 }
