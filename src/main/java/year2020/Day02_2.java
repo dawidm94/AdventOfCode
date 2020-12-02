d8cb7b623e79c13178b6a71d94d3a06f018f21b2
Dawid Marcinków <dawid.marcinkow@gmail.com>
2020-12-02 08:45:39 +0100

    [AoC2020] Day 2 second task

---
+++ src/main/java/year2020/Day02_2.java
@@ -0,0 +1,53 @@
+package main.java.year2020;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day02_2 {
+
+	private final static String INPUT_PATH = "src/main/resources/2020Day02Input.txt";
+	private final static String PASSWORD_POLICY_REGEX = "^(\\d+)-(\\d+)\\s(.):\\s(.+)$";
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File(INPUT_PATH);
+		int numberOfValidPasswords = 0;
+		int numberOfInvalidPasswords = 0;
+		Scanner scanner = new Scanner(file);
+		while (scanner.hasNextLine()) {
+			boolean isValid = processPassword(scanner.nextLine());
+			if (isValid) {
+				numberOfValidPasswords++;
+			} else {
+				numberOfInvalidPasswords++;
+			}
+		}
+		System.out.println("CHECKED ALL PASSWORDS");
+		System.out.printf("VALID: %d \r\n", numberOfValidPasswords);
+		System.out.printf("INVALID: %d", numberOfInvalidPasswords);
+
+	}
+
+	private static boolean processPassword(String nextLine) {
+		Pattern r = Pattern.compile(PASSWORD_POLICY_REGEX);
+		Matcher m = r.matcher(nextLine);
+		if (m.find()) {
+			int firstPosition = Integer.parseInt(m.group(1)) - 1;
+			int secondPosition = Integer.parseInt(m.group(2)) - 1;
+			char expectedChar = m.group(3).charAt(0);
+			String password = m.group(4);
+			return validPassword(firstPosition, secondPosition, expectedChar, password);
+		} else {
+			System.out.printf("Something wrong. Could find regex '%s' for line '%s'", PASSWORD_POLICY_REGEX, nextLine);
+			return false;
+		}
+	}
+
+	private static boolean validPassword(int firstPosition, int secondPosition, char expectedChar, String password) {
+		boolean firstValidation = password.charAt(firstPosition) == expectedChar;
+		boolean secondValidation = password.charAt(secondPosition) == expectedChar;
+		return firstValidation != secondValidation;
+	}
+}
