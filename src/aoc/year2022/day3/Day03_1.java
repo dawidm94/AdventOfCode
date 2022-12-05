94cb2148924aeaf09936f06d1fc1e425636e0835
Dawid Marcinków <dawid.marcinkow@gmail.com>
2022-12-05 16:19:59 +0100

    Aod 2022/DAY3_1

---
+++ src/aoc/year2022/day3/Day03_1.java
@@ -0,0 +1,34 @@
+package aoc.year2022.day3;
+
+import aoc.AocUtils;
+
+import java.io.IOException;
+import java.util.List;
+
+import static aoc.model.AocDay.DAY_1;
+import static aoc.model.AocDay.DAY_3;
+import static aoc.model.AocYear.YEAR2022;
+
+public class Day03_1 {
+	private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	public static void main(String[] args) throws IOException {
+		int result = 0;
+		List<String> lines = AocUtils.getAllStringLines(YEAR2022, DAY_3);
+
+		for (String line: lines) {
+			int lineLength = line.length();
+			String firstCompartment = line.substring(0, lineLength / 2);
+			String secondCompartment = line.substring(lineLength / 2, lineLength);
+
+			for (Character sign: firstCompartment.toCharArray()) {
+				if (secondCompartment.contains(sign.toString())) {
+					result += (ALPHABET.indexOf(sign) + 1);
+					break;
+				}
+			}
+		}
+
+		System.out.println("Result is: " + result);
+	}
+}
